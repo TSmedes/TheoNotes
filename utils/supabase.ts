e090cc41670566c81b0c738ce62a89514f3f7a25
TSmedes <144851348+TSmedes@users.noreply.github.com>
2024-09-12 09:39:29 -0700

    Update supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -5,8 +5,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 export const supabase = createClient(
-    'https://zfmugsyfplrzchrovtlw.supabase.co', 
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmbXVnc3lmcGxyemNocm92dGx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIxOTcyNDEsImV4cCI6MjAzNzc3MzI0MX0.MtYBMPk6iH8BgKB9RxH66kR_HsB0h2hb0z-QYEFI2eQ', 
+    'REDACTED', 
+    'REDACTED', 
     
 {
   auth: {
